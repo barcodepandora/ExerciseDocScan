d8215decf5d49eb0bac6b23409ff56a58658765c
Juan <jmoreno@novopayment.com>
2024-07-18 11:43:38 -0500

    Stub document scan as a framework

---
+++ FrameworkDocScannin/FrameworkDocScannin/FrameworkDocScannin-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "InC.h"
+#import "DocScan.hpp"
+#import "Point.h"
+#import <Foundation/Foundation.h>
